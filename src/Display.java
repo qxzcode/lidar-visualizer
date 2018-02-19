6b5420200fa05694538cf2a5084af6b559de413e
qxzcode <qxzcraft@gmail.com>
2018-02-18 20:21:22 -0800

    Moved ICP to its own file

--- src/Display.java
+++ src/Display.java
@@ -20,7 +20,10 @@ public class Display extends JPanel {
     
     private JLabel display;
     
-    private Point[] points;
+    public Point[] points;
+    
+    public final ICP icp;
+    public static final boolean SINGLE_STEP = true;
     
     public static final double FPS = 10;
     public Display() {
@@ -39,17 +42,7 @@ public class Display extends JPanel {
         
         setFocusable(true);
         requestFocus();
-        setVisible(true); 
-        
-        // Creating Points
-        points = generateArray();
-        calcBounds();
-        doICP(reference, SINGLE_STEP? 0 : 5000);
-        new javax.swing.Timer((int)(1000/FPS), evt -> {
-            drawRev++;
-            if (drawRev >= numRevs) drawRev = 0;
-            repaint();
-        }).start();
+        setVisible(true);
         
         MouseAdapter mouseListener = new MouseAdapter() {
             @Override
@@ -77,6 +70,17 @@ public class Display extends JPanel {
                 onKey(e.getKeyCode());
             }
         });
+        
+        // Creating Points
+        points = generateArray();
+        calcBounds();
+        icp = new ICP(this);
+        icp.doICP(SINGLE_STEP? 0 : 5000);
+        new javax.swing.Timer((int)(1000/FPS), evt -> {
+            drawRev++;
+            if (drawRev >= numRevs) drawRev = 0;
+            repaint();
+        }).start();
     }
     
     int selectedI = -1;
@@ -115,7 +119,7 @@ public class Display extends JPanel {
         if (code == KeyEvent.VK_EQUALS || code == KeyEvent.VK_PLUS)
             scaleFactor *= ZOOM_RATE;
         if (code == KeyEvent.VK_SPACE)
-            doICP(reference, 1);
+            icp.doICP(1);
         if (code == KeyEvent.VK_L)
             connectPoints = !connectPoints;
         calcBounds();
@@ -130,107 +134,6 @@ public class Display extends JPanel {
         float[] rgb = col.getRGBColorComponents(null);
         return new Color(rgb[0],rgb[1],rgb[2], alpha);
     }
-    
-    //////////////////////////// ICP ////////////////////////////
-    
-    class PointPair {
-        public Point a, b;
-        double dist;
-        public PointPair(Point a, Point b, double dist) {
-            this.a = a;
-            this.b = b;
-            this.dist = dist;
-        }
-    }
-    
-    public ReferenceModel reference = ReferenceModel.TOWER;
-    private ReferenceModel transReference;
-    private Transform icpTrans = new Transform(0.0, 7000, -2000);
-    
-    public static final double OUTLIER_THRESH = 1.9; // multiplier of mean
-    public static final boolean SINGLE_STEP = true;
-    private ArrayList<PointPair> pairs = new ArrayList<>();
-    double dbgLength;
-    private double lastMean = Double.POSITIVE_INFINITY;
-    public Transform doICP(ReferenceModel reference, int iterations) {
-        debug("Doing ICP registration ("+iterations+" iters)...");
-        long startTime = System.nanoTime();
-        Transform trans = icpTrans;
-        for (int n = 0; n < iterations+1; n++) {
-            /// get pairs of corresponding points
-            Transform transInv = trans.inverse();
-            pairs.clear();
-            
-            double sumDists = 0;
-            final double threshold = lastMean*OUTLIER_THRESH;
-            dbgLength = threshold;
-            
-            double SumXa = 0, SumXb = 0, SumYa = 0, SumYb = 0;
-            double Sxx = 0, Sxy = 0, Syx = 0, Syy = 0;
-            for (Point p : points) {
-                Point p2 = transInv.apply(p);
-                Point rp = reference.getClosestPoint(p2);
-                double dist = p2.getDistance(rp);
-                sumDists += dist;
-                
-                p.good = dist < threshold;
-                if (!p.good) continue;
-                pairs.add(new PointPair(p, rp, dist));
-                
-                // Compute the terms:
-                SumXa += p.x;
-                SumYa += p.y;
-                
-                SumXb += rp.x;
-                SumYb += rp.y;
-                
-                Sxx += p.x * rp.x;
-                Sxy += p.x * rp.y;
-                Syx += p.y * rp.x;
-                Syy += p.y * rp.y;
-            }
-            
-            if (n==iterations) break;
-            lastMean = sumDists / points.length;
-            
-            /// calculate the new transform
-            // code based on http://mrpt.ual.es/reference/devel/se2__l2_8cpp_source.html#l00158
-            final int N = pairs.size();
-            if (N==0) return new Transform(); // TODO: handle this better, or avoid it
-            final double N_inv = 1.0 / N;
-            
-            final double mean_x_a = SumXa * N_inv;
-            final double mean_y_a = SumYa * N_inv;
-            final double mean_x_b = SumXb * N_inv;
-            final double mean_y_b = SumYb * N_inv;
-            
-            // Auxiliary variables Ax,Ay:
-            final double Ax = N * (Sxx + Syy) - SumXa * SumXb - SumYa * SumYb;
-            final double Ay = SumXa * SumYb + N * (Syx - Sxy) - SumXb * SumYa;
-            
-            final double theta = (Ax == 0 && Ay == 0)? 0.0 : Math.atan2(Ay, Ax);
-            
-            final double ccos = Math.cos(theta);
-            final double csin = Math.sin(theta);
-            
-            final double tx = mean_x_a - mean_x_b * ccos + mean_y_b * csin;
-            final double ty = mean_y_a - mean_x_b * csin - mean_y_b * ccos;
-            
-            if (theta==trans.theta && tx==trans.tx && ty==trans.ty) {
-                debug("Converged on iteration n="+n);
-                break;
-            }
-            trans = new Transform(theta, tx, ty, csin, ccos);
-        }
-        long endTime = System.nanoTime();
-        debug("Done ("+Math.round((endTime-startTime)/1000000f)+" ms)");
-        debug(trans);
-        icpTrans = trans;
-        transReference = trans.apply(reference);
-        return trans;
-    }
-    
-    /////////////////////////////////////////////////////////////
     
     private double scale;
     private int centerX, centerY;
@@ -277,8 +180,8 @@ public class Display extends JPanel {
         
         if (DRAW_ICP) {
             g.setColor(Color.RED);
-            for (PointPair pair : pairs) {
-                drawLine(g, pair.a, icpTrans.apply(pair.b));
+            for (ICP.PointPair pair : icp.pairs) {
+                drawLine(g, pair.a, icp.icpTrans.apply(pair.b));
             }
         }
         
@@ -306,11 +209,11 @@ public class Display extends JPanel {
         }
         
         if (DRAW_ICP)
-            transReference.draw(g);
+            icp.transReference.draw(g);
         
         if (selectedI >= 0 && DRAW_ICP) {
             Point p = points[selectedI];
-            Point rp = transReference.getClosestPoint(p);
+            Point rp = icp.transReference.getClosestPoint(p);
             g.setColor(Color.RED);
             drawLine(g, p, rp);
         }
@@ -318,7 +221,7 @@ public class Display extends JPanel {
         int[] originPos = getDrawLoc(Point.fromRect(0, 0));
         g.setColor(Color.WHITE);
         g.drawOval(originPos[0]-4, originPos[1]-4, 8, 8);
-        g.drawLine(20, 20, 20+(int)(dbgLength*scale), 20);
+        g.drawLine(20, 20, 20+(int)(icp.dbgLength*scale), 20);
     }
     
     public static final int REVS_TO_READ = 5000;
---
+++ src/ICP.java
@@ -0,0 +1,108 @@
+import java.util.ArrayList;
+
+public class ICP {
+    
+    private final Display disp;
+    
+    public ICP(Display d) {
+        disp = d;
+    }
+    
+    
+    public static class PointPair {
+        public Point a, b;
+        double dist;
+        public PointPair(Point a, Point b, double dist) {
+            this.a = a;
+            this.b = b;
+            this.dist = dist;
+        }
+    }
+    
+    public ReferenceModel reference = ReferenceModel.TOWER;
+    public ReferenceModel transReference;
+    public Transform icpTrans = new Transform(0.0, 7000, -2000);
+    
+    public static final double OUTLIER_THRESH = 1.9; // multiplier of mean
+    public ArrayList<PointPair> pairs = new ArrayList<>();
+    public double dbgLength;
+    public double lastMean = Double.POSITIVE_INFINITY;
+    public Transform doICP(int iterations) {
+        disp.debug("Doing ICP registration ("+iterations+" iters)...");
+        long startTime = System.nanoTime();
+        Transform trans = icpTrans;
+        for (int n = 0; n < iterations+1; n++) {
+            /// get pairs of corresponding points
+            Transform transInv = trans.inverse();
+            pairs.clear();
+            
+            double sumDists = 0;
+            final double threshold = lastMean*OUTLIER_THRESH;
+            dbgLength = threshold;
+            
+            double SumXa = 0, SumXb = 0, SumYa = 0, SumYb = 0;
+            double Sxx = 0, Sxy = 0, Syx = 0, Syy = 0;
+            for (Point p : disp.points) {
+                Point p2 = transInv.apply(p);
+                Point rp = reference.getClosestPoint(p2);
+                double dist = p2.getDistance(rp);
+                sumDists += dist;
+                
+                p.good = dist < threshold;
+                if (!p.good) continue;
+                pairs.add(new PointPair(p, rp, dist));
+                
+                // Compute the terms:
+                SumXa += p.x;
+                SumYa += p.y;
+                
+                SumXb += rp.x;
+                SumYb += rp.y;
+                
+                Sxx += p.x * rp.x;
+                Sxy += p.x * rp.y;
+                Syx += p.y * rp.x;
+                Syy += p.y * rp.y;
+            }
+            
+            if (n==iterations) break;
+            lastMean = sumDists / disp.points.length;
+            
+            /// calculate the new transform
+            // code based on http://mrpt.ual.es/reference/devel/se2__l2_8cpp_source.html#l00158
+            final int N = pairs.size();
+            if (N==0) return new Transform(); // TODO: handle this better, or avoid it
+            final double N_inv = 1.0 / N;
+            
+            final double mean_x_a = SumXa * N_inv;
+            final double mean_y_a = SumYa * N_inv;
+            final double mean_x_b = SumXb * N_inv;
+            final double mean_y_b = SumYb * N_inv;
+            
+            // Auxiliary variables Ax,Ay:
+            final double Ax = N * (Sxx + Syy) - SumXa * SumXb - SumYa * SumYb;
+            final double Ay = SumXa * SumYb + N * (Syx - Sxy) - SumXb * SumYa;
+            
+            final double theta = (Ax == 0 && Ay == 0)? 0.0 : Math.atan2(Ay, Ax);
+            
+            final double ccos = Math.cos(theta);
+            final double csin = Math.sin(theta);
+            
+            final double tx = mean_x_a - mean_x_b * ccos + mean_y_b * csin;
+            final double ty = mean_y_a - mean_x_b * csin - mean_y_b * ccos;
+            
+            if (theta==trans.theta && tx==trans.tx && ty==trans.ty) {
+                disp.debug("Converged on iteration n="+n);
+                break;
+            }
+            trans = new Transform(theta, tx, ty, csin, ccos);
+        }
+        long endTime = System.nanoTime();
+        disp.debug("Done ("+Math.round((endTime-startTime)/1000000f)+" ms)");
+        disp.debug(trans);
+        icpTrans = trans;
+        transReference = trans.apply(reference);
+        return trans;
+    }
+    
+}
